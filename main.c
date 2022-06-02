be7cc29bce951608b2188a830adef979f4f0e018
SHIVAM <93755657+RESOSHIVAM@users.noreply.github.com>
2022-06-02 00:41:59 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a,b,k;
+   float c,d;
+   char x,y,z;
+   x='A';
+   y='C';
+   k=x+y;
+   z=x+y;
+   printf("%d",k);
+   printf("%f",z);
+    return 0;
+}
